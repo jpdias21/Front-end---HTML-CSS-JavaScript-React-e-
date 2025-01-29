d0f58b21f8df34afd9799aed25bf1c6ba55ba977
Joao Pedro <joaopedrodiasde@gmail.com>
2025-01-29 16:55:54 -0300

    Adiconando intro de javascript 29/01/2025 5 horas de estudos

---
+++ JavaScript/2_Operadores/js/index.js
@@ -0,0 +1,62 @@
+
+// Number
+
+console.log(typeof 2)
+console.log(typeof 15)
+console.log(typeof -8734)
+
+//Operações Aritmeticas
+console.log(5+19)
+console.log(1238*9)
+console.log(823-73)
+console.log(5 + (12*77))
+
+//3- Special numbers 
+
+console.log(typeof Infinity)
+console.log(typeof -Infinity)
+console.log(15 * "Joao") 
+
+// strings
+console.log(`Este aqui e com crase`)
+console.log("Este aqui e com aspas duplas")
+console.log("13")
+
+//Concatenação 
+let textoUm = ('Joao')
+let textoDois= ('Pedro')
+console.log(textoUm + textoDois)
+
+console.log('Meu ' + 'nome ' + "e " + ':')
+
+//Interpolação 
+let nome = ('Joao Pedro')
+console.log(`Meu nome e ${nome}`)
+
+console.log(`A soma de 1 milhão de bitcoin mais o seu valor e ${100000 * 100000}`)
+
+
+//Bolean
+console.log(1===1)
+console.log(1 > 10)
+console.log(1=== 'Joao')
+console.log(1 < 10)
+console.log(100 > 1)
+
+//Comparação
+console.log(5>= 3)
+console.log(10 <= 9)
+console.log( 10 != 100)
+console.log(10 === 10)
+console.log(10 === 9)
+
+//Comparação de indenticos
+console.log(9 === '9')
+console.log(10 !== "10")
+
+//Operadores logicos 
+console.log(10 >5 && 50 < 100)
+console.log(50 < 100 && 'Joao' === 1)
+console.log(10 >= 9 || 'matheus' === 2)
+
+
